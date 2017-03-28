8f17dac733a2b660c4569e06add35c20a9460be4
Jean-Tiare Le Bigot <jean-tiare.le-bigot@corp.ovh.com>
2017-03-28 15:36:39 +0200

    branding

--- src/app/app.js
+++ src/app/app.js
@@ -45,7 +45,7 @@ angular.module( 'consoleApp', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | My Little API' ;
+      $scope.pageTitle = toState.data.pageTitle + ' | OVH API Console' ;
     }
   });
 })
