c51e77fe2b91a1e67104ca0af7d68e6df6b99eca
Jean-Tiare Le Bigot <jean-tiare.le-bigot@corp.ovh.com>
2017-03-31 12:04:14 +0200

    do not send boolean param when null

--- src/app/api/api.service.js
+++ src/app/api/api.service.js
@@ -363,7 +363,7 @@ angular.module('consoleApp').service('Api', function ($rootScope, $q, Ovh, $http
                 var paramValue = getRequestParamValue(_param);
 
                 // If the value evaluates to false, prune it, unless it is mandatory or explicitely marked as null by the user
-                if (paramValue || _param.required || _param.inputMode !== 'input') {
+                if (paramValue || paramValue === false || param.required || (param.inputMode !== 'input' && param.inputMode !== undefined)) {
                     ret[_param.name] = paramValue;
                 }
             });
@@ -392,7 +392,7 @@ angular.module('consoleApp').service('Api', function ($rootScope, $q, Ovh, $http
             }
 
             // If the value evaluates to false, prune it, unless it is mandatory or explicitely marked as null by the user
-            if (paramValue || param.required || param.inputMode !== 'input') {
+            if (paramValue || paramValue === false || param.required || (param.inputMode !== 'input' && param.inputMode !== undefined)) {
                 if (!config[paramCategory]) {
                     config[paramCategory] = {};
                 }
